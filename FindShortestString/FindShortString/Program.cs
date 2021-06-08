9bbfdd6475886867ee8135ff0e9b5b5b5e4a02de
Paul <p.andrews@outlook.es>
2021-06-08 09:07:06 +0200

    Added 2 projects
    Added Regex and ShortestString

---
+++ FindShortestString/FindShortString/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+/*
+ * Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+namespace FindShortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "bitcoin take over the world maybe who knows perhaps";
+
+            Console.WriteLine("Shortest word has: {0}", FindShort(phrase));
+
+
+        }
+
+        public static int FindShort(string word)
+        {
+            string[] array = word.Split(" ");
+
+            int num1 = array[0].Length;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+
+                int num = array[i].Length;
+               
+
+                if(num < num1)
+                {
+                    num1 = num;
+                    string shortWord = array[i];
+                    Console.WriteLine("New shortest word is: \"{0}\" with a length of {1}", shortWord, num1);
+                }
+
+            }
+
+            return num1;
+        }
+
+        //Splits the string into an array, establishes maxx as the max value possible of an int, foreach recorre the array, checks if the length of the value is less than the maxx, if it is
+        // it changes the max value and returns it.
+        /*
+         * public static int FindShort(string s)
+    {
+                string[] strArr = s.Split(' ');
+                int maxx = int.MaxValue;
+
+                    foreach (string item in strArr)
+                        {
+                             if (item.Length < maxx)
+                                {
+                                      maxx = item.Length;
+                                }
+                            }
+                            return maxx;
+    }*/
+    }
+}
---
+++ Regex/RegexProgram/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+// https://www.tutorialspoint.com/csharp/csharp_regular_expressions.htm
+// https://docs.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
+// https://docs.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.regex?view=net-5.0
+// https://docs.microsoft.com/en-us/dotnet/standard/base-types/anchors-in-regular-expressions
+
+
+
+namespace RegexProgram
+{
+
+
+    /*
+     * ^ = start at beginning of string.
+     * \d = match any decimal digit.
+     * { } = amount if digits.
+     * | = or
+     * \z = match must occur at end of string only.
+     * */
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string pin = null;
+            //string pin = "1234\n";
+            do
+            {
+                Console.WriteLine("Enter a 4 or 6 digit number...");
+                pin = Console.ReadLine();
+            } while (CheckPattern(pin));
+
+            Console.WriteLine("Caracteres introducidos incorrectos");
+
+            //Console.WriteLine(CheckPattern(pin));
+            Console.ReadKey();
+        }
+
+        public static bool CheckPattern(string pin)
+        {
+
+            return Regex.IsMatch(pin, @"^(\d{6}|\d{4})\z");
+
+        }
+    }
+}
